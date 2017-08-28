ea198e8c06300496f4d5f5d49863b46c708d51e6
joshua1988 <jangkeehyo@gmail.com>
2017-08-28 17:46:12 +0900

    Updated the push sender server logic with latest spec on FCM

--- firebase-function/functions/test.js
+++ firebase-function/functions/test.js
@@ -1,23 +1,27 @@
+// @@ Firebase Clouding Messaging HTTP Server Protocol Spec
+// https://firebase.google.com/docs/cloud-messaging/http-server-ref#notification-payload-support
 const request = require('request-promise');
 
-const pushURL = "https://android.googleapis.com/gcm/send";
+// const pushURL = "https://android.googleapis.com/gcm/send";
+const FCMPushURL = "https://fcm.googleapis.com/fcm/send";
 const FCMPushServerKey = "key=" + "AAAAKGUavjw:APA91bGaFUTRfbIeuNUE3DvaOAfJxiayFtaHNDITMA5F32jnjHcivRthgP6h7yNt-BuGtpUg2ESqwXq22juEUeWfo9fG8ne1wlAZoHwlRWiv2YjdE88JcNguLv-n8418RMpxE8ARlfsV";
 
 function makePostRequest() {
   return request({
-    uri: pushURL,
+    uri: FCMPushURL,
     method: 'POST',
     headers: {
       'Authorization' : FCMPushServerKey
     },
     json: true,
     body: {
-      "registration_ids" : ["em7OFi2pb0g:APA91bHMJ_Z8Q10cI3I5-SptWGLqFS4CU0R9CtLuxPKpn_FJfV5SWoJkhwMe5aFkz8fdCQ10G9fGSd2y_XOi6QPYpSD62LbRvacAYt0Bk7QHviuReSK5K9JaFkcZ2XiTvF85DSWC73kj"],
-      "data": {
-        "post_title": "This is a Firebase Cloud Messaging Topic Message!",
-        "post_content": "This is the content",
-        "post_url": "current url"
-      }
+      "registration_ids": ["fQ16LSSwDyM:APA91bFVnPyEV5uQ_pNc3esuuH30_gRxkh5xSXK0rI0yFV6_OTt_J07V0LOXFzVCJTM1Cf2BLe7ecV3n0egW0PBbTTrB8wY2CRMkdl6rDwe_UpziEiax_k8_kMg7Xm5fIQouc5RYZ1X1"],
+      "notification": {
+         "title": "Portugal vs. Denmark",
+         "body": "5 to 1",
+         "icon": "firebase-logo.png",
+         "click_action": "http://localhost:8081"
+       },
     }
     // resolveWithFullResponse: true
   }).then(response => {
