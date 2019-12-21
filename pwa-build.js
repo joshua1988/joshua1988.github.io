44b9532689f036c200ccadbc0433232c78cf5617
joshua1988 <jangkeehyo@gmail.com>
2019-12-21 15:46:56 +0900

    add pwa related files

--- pwa-build.js
+++ pwa-build.js
@@ -16,11 +16,39 @@ generateSW({
     "**/demo.html",
     "js/pwa-push.js",
     "package.json",
-    "package-lock.json"
+    "package-lock.json",
+    "preflight_.png"
+  ],
+  cleanupOutdatedCaches: true,
+  // Increase the limit to 4mb:
+  maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+  // Define runtime caching rules.
+  runtimeCaching: [
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+      // Apply a cache-first strategy.
+      handler: "CacheFirst",
+
+      options: {
+        // Use a custom cache name.
+        cacheName: "images",
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 10
+        }
+      }
+    }
   ]
   // Other configuration options...
-}).then(({ count, size }) => {
-  console.log(
-    `Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`
-  );
-});
+})
+  .then(({ count, size }) => {
+    console.log(
+      `Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`
+    );
+  })
+  .catch(error => {
+    console.log("# PWA Build ERROR - ", error);
+  });
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "_site/",
+  "globPatterns": [
+    "**/*.{html,js,png,xml,sh,css,jpg,ico,json,eot,svg,ttf,woff,woff2,otf,jpeg,key,gif}"
+  ],
+  "swDest": "_site/sw.js"
+};
