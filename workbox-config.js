538c2092a7b4c7462537f4beea27bdd2c8436b4c
joshua1988 <jangkeehyo@gmail.com>
2021-01-10 23:35:28 +0900

    update the build script along with excluding shell scripts

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,19 @@
 module.exports = {
   "globDirectory": "_site/",
   "globPatterns": [
-    "**/*.{html,js,png,xml,sh,css,jpg,ico,json,eot,svg,ttf,woff,woff2,otf,jpeg,key,gif}"
+    "**/*.{html,js,png,css,jpg,ico,json,eot,svg,ttf,woff,woff2,otf,jpeg,key,gif}"
   ],
   "swSrc": "sw-custom.js",
-  "swDest": "_site/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "_posts/_backup/**",
+    "workbox-config.js",
+    "pwa-build.js",
+    "firebase-function/**",
+    "**/demo.html",
+    "js/pwa-push.js",
+    "package.json",
+    "package-lock.json",
+    "preflight_.png"
+  ],
 };
